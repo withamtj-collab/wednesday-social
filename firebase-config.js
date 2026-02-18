d31ce627a7011093d88be81596afbcd5e334c299
withamtj-collab <withamtj@gmail.com>
2026-02-18 13:54:09 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// ═══════════════════════════════════════════════════════════════
+// 🔧 YOUR FIREBASE CONFIG — Edit these values with your own
+// ═══════════════════════════════════════════════════════════════
+const firebaseConfig = {
+  apiKey: "AIzaSyBX69goNK8vbScUciBJCa1QJSaxu1KXlKo",
+  authDomain: "wednesday-social-c3a11.firebaseapp.com",
+  databaseURL: "https://wednesday-social-c3a11-default-rtdb.firebaseio.com",
+  projectId: "wednesday-social-c3a11",
+  storageBucket: "wednesday-social-c3a11.firebasestorage.app",
+  messagingSenderId: "466466155926",
+  appId: "1:466466155926:web:5ae46c763dea00701e81b7",
+  measurementId: "G-K3CQN6QB69"
+};
